94f1a1194827a7387fedef1067712d4c8284415e
Noobsaure <Noobsaure@gmail.com>
2013-02-12 23:07:53 +0100

    Heuristique
    Après une heure d'arrachage de cheveux

--- mygdxgame/src/com/me/mygdxgame/ia/pathfinding/heuristics/ManhattanHeuristic.java
+++ mygdxgame/src/com/me/mygdxgame/ia/pathfinding/heuristics/ManhattanHeuristic.java
@@ -4,32 +4,16 @@ import com.me.mygdxgame.ia.pathfinding.AStarHeuristic;
 import com.me.mygdxgame.ia.pathfinding.Mover;
 import com.me.mygdxgame.ia.pathfinding.TileBasedMap;
 
-
-/**
- * A heuristic that drives the search based on the Manhattan distance
- * between the current location and the target
- * 
- * @author Kevin Glass
- */
 public class ManhattanHeuristic implements AStarHeuristic {
-	/** The minimum movement cost from any one square to the next */
+
 	private int minimumCost;
 	
-	/**
-	 * Create a new heuristic 
-	 * 
-	 * @param minimumCost The minimum movement cost from any one square to the next
-	 */
 	public ManhattanHeuristic(int minimumCost) {
 		this.minimumCost = minimumCost;
 	}
-	
-	/**
-	 * @see AStarHeuristic#getCost(TileBasedMap, Mover, int, int, int, int)
-	 */
+
 	public float getCost(TileBasedMap map, Mover mover, int x, int y, int tx,
 			int ty) {
-		return minimumCost * (Math.abs(x-tx) + Math.abs(y-ty));
+		return minimumCost * Math.max(Math.abs(y-ty),2*Math.abs(x-tx)-Math.abs((y % 2) - (ty % 2)));
 	}
-
 }
