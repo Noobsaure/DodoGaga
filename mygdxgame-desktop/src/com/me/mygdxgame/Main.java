da41ef5baebef82763ca8e4a51d6404d8baef945
Gaetan <gbproduction.rpg@gmail.com>
2013-03-04 12:20:30 +0100

    lala

--- mygdxgame-desktop/src/com/me/mygdxgame/Main.java
+++ mygdxgame-desktop/src/com/me/mygdxgame/Main.java
@@ -4,12 +4,14 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class Main {
-   public static void main(String[] args) {
-      LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-      cfg.title = "Drop";
-      cfg.useGL20 = true;
-      cfg.width = 1200;
-      cfg.height = 800;
-      new LwjglApplication(new App(), cfg);
-   }
+	
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "Drop";
+		cfg.useGL20 = true;
+		cfg.width = 1200;
+		cfg.height = 800;
+		new LwjglApplication(new App(), cfg);
+	}
+
 }
